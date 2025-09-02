9735ebecc2e0381c92e3dcb48053fbb2b8a8ef77
NisalMallawaarachchi <nisalmallawarachchi@gmail.com>
2025-09-02 19:18:41 +0530

    fix(auth): update default value of isApproved to true in owner schema

--- backend/Models/ownerModel.js
+++ backend/Models/ownerModel.js
@@ -39,7 +39,7 @@ const ownerSchema = new mongoose.Schema(
 
     isApproved: {
       type: Boolean,
-      default: false,
+      default: true,
       description: "Indicates whether the owner is approved by super admin",
     },
 
